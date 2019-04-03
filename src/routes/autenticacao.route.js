d1ea834f6b0bfe82c50cff882a442fff040fc16d
Tiago <tiagosilveira1998@gmail.com>
2019-04-03 00:56:28 -0300

    Cors

--- src/routes/autenticacao.route.js
+++ src/routes/autenticacao.route.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
-    app.post('/autenticar', cors(), function (req, res) {
+    app.post('/autenticar', function (req, res) {
         app.src.controllers.autenticacao.autenticar(app, req, res)
     });
 }
